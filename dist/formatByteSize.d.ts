d850c7100eece5b72ff69c74af6ab388fa48c3c9
Kris Herlaar <kris@theredhead.nl>
2021-03-03 22:07:58 +0100

    removed dist

--- dist/formatByteSize.d.ts
+++
@@ -1,4 +0,0 @@
-export declare type FormatByteSizePostProcessorFunc = (num: number, unit: string) => string;
-export declare const stripPointZeroZero: FormatByteSizePostProcessorFunc;
-export declare const alwaysTwoDecimals: FormatByteSizePostProcessorFunc;
-export declare const formatByteSize: (numberOfBytes: number, kilo?: 1000 | 1024, postProcess?: FormatByteSizePostProcessorFunc) => string;
